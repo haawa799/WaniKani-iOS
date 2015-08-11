2cc3ae8f95b546d19a3dee1abf05da483d28bb0e
Andriy K <haawaplus@gmail.com>
2015-08-11 10:04:10 +0300

    Initial Commit

---
+++ WaniKani/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaniKani
+//
+//  Created by Andriy K. on 8/11/15.
+//  Copyright (c) 2015 Andriy K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
