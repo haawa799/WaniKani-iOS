b5d6194dd63c4e161b17975acd2623b3fd998d02
Andriy K <haawaplus@gmail.com>
2015-08-11 10:59:27 +0300

    First request tryout

--- WaniKani/ViewController.swift
+++ WaniKani/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    WaniApiManager.qqq()
+    WaniApiManager.sharedInstance.qqq()
   }
 
   override func didReceiveMemoryWarning() {
--- WaniKit/WaniApiManager.swift
+++ WaniKit/WaniApiManager.swift
@@ -6,12 +6,20 @@
 //
 //
 
-import UIKit
+import Alamofire
 
 public class WaniApiManager: NSObject {
   
-  public class func qqq() {
-    println("QQQ")
+  public static let sharedInstance = WaniApiManager()
+  
+  private static let baseURL = "https://www.wanikani.com/api/"
+  private static let myKey = "c6ce4072cf1bd37b407f2c86d69137e3"
+  
+  public func qqq() {
+    Alamofire.request(.GET, "https://www.wanikani.com/api/user/c6ce4072cf1bd37b407f2c86d69137e3/study-queue", parameters: nil)
+      .responseJSON(options: NSJSONReadingOptions.AllowFragments) { (_, _, JSON, _) -> Void in
+        println(JSON)
+    }
   }
   
 }
